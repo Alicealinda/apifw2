09540586413af0114d69bb8f3831e35abeedf056
Alicealinda <149427831+Alicealinda@users.noreply.github.com>
2025-12-15 17:08:32 -0400

    Add files via upload

--- rotas/postnoticias.js
+++ rotas/postnoticias.js
@@ -1,25 +1,28 @@
-module.exports =(app)=>{
-    app.post("/postnoticias",async (req,res) => {
-        try {const titulonoticia = req.body.titulonoticia
-        const conteudonoticia = req.body.conteudonoticia
-        const tiponoticia = req.body.tiponoticia
-        await app.dbClient.connect();
-        res.send("ola")
-        }catch(erro){
+module.exports = (app) => {
+    app.post("/postnoticias", async (req, res) => {
+        try {
+            const titulonoticia = req.body.titulonoticia
+            const conteudonoticia = req.body.conteudonoticia
+            const tiponoticia = req.body.tiponoticia
+            await app.dbClient.connect();
+
+            const resultado = await app.dbClient.db("portalnoticias")
+                .collecion("noticias")
+                .insertOne({
+                    titulonoticia: titulonoticia,
+                    conteudonoticia: conteudonoticia,
+                    tiponoticia: tiponoticia,
+                    datahoracadastro: new Date()
+                })
+            res.status(200).send("noticias gravadas com sucesso!")
+        } catch (erro) {
             res.status(400).send(erro)
         }
-       /*  try {
-            const resultado =  await app.dbClient.db("portalnoticias")
-            .collecion("noticias")
-            .insertOne({titulonoticia:titulonoticia,
-                conteudonoticia:conteudonoticia,
-                tiponoticia:tiponoticia,
-                datahoracadastro:new Date()})
-            res.status(200).send("noticias gravadas com sucesso!")
-        } catch (error) {
-            res.status(400).send("erro ao gravar a noticias:",error)
-            
-        } */
-        
+        /*  try {
+         } catch (error) {
+             res.status(400).send("erro ao gravar a noticias:",error)
+             
+         } */
+
     })
 }
