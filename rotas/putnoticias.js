eae10b2040261615e34f85eb1736fde7f053a4b6
Alicealinda <149427831+Alicealinda@users.noreply.github.com>
2025-12-15 17:20:53 -0400

    Add files via upload

--- rotas/putnoticias.js
+++ rotas/putnoticias.js
@@ -1,23 +1,23 @@
-const { ObjectId } = require("mongodb");
-module.exports =(app)=>{
-    app.put("/putnoticias",async (req,res) => {
+const { ObjectId } = require("mongodb")
+module.exports = (app) => {
+    app.put("/putnoticias", async (req, res) => {
         try {
-            const id = ObjectId.createFromHexString(req.body._id);
+            const _id = ObjectId.createFromHexString(req.body._id)
             const titulonoticia = req.body.titulonoticia
-            const conteudonoticia = rep.body.conteudonoticia
+            const conteudonoticia = req.body.conteudonoticia
             const tiponoticia = req.body.tiponoticia
-            await app.dbClient.connect();
-            const resultado =  await app.dbClient.db('portalnoticias')
-            .collecion('noticias')
-            .updateOne({_id:id},{$set:{titulonoticia:titulonoticia,
-                conteudonoticia:conteudonoticia,
-                tiponoticia:tiponoticia,
-                datahoracadastro:new Date()}})
-            res.status(200).send("noticias gravadas com sucesso!")
+            await app.dbClient.connect()
+            const resultado = await app.dbClient.db('portalnoticias')
+                .collection('noticias')
+                .updateOne({ _id: _id },
+                    {
+                        $set: { titulonoticia: titulonoticia, conteudonoticia: conteudonoticia, tiponoticia: tiponoticia }
+                    })
+            if (!resultado.modifiedCount) {
+                throw new Error();
+            } else { res.status(200).json({ status: 1 }) }
         } catch (error) {
-            res.status(400).send("erro ao gravar a noticias:",error)
-            
+            res.status(400).json({ status: 0 })
         }
-        
     })
 }
