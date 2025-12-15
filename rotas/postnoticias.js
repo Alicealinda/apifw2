fc29065d812e0f39979e75f7fce47ee2359df411
Alicealinda <149427831+Alicealinda@users.noreply.github.com>
2025-12-15 17:14:05 -0400

    Add files via upload

--- rotas/postnoticias.js
+++ rotas/postnoticias.js
@@ -6,14 +6,14 @@ module.exports = (app) => {
             const tiponoticia = req.body.tiponoticia
             await app.dbClient.connect();
 
-            const resultado = await app.dbClient.db("portalnoticias")
+/*             const resultado = await app.dbClient.db("portalnoticias")
                 .collecion("noticias")
                 .insertOne({
                     titulonoticia: titulonoticia,
                     conteudonoticia: conteudonoticia,
                     tiponoticia: tiponoticia,
                     datahoracadastro: new Date()
-                })
+                }) */
             res.status(200).json("noticias gravadas com sucesso!")
         } catch (erro) {
             res.status(400).json(erro)
