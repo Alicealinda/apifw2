9a5b513128accc9e91513e943a04dd67a721b4b7
Alicealinda <149427831+Alicealinda@users.noreply.github.com>
2025-12-15 17:06:46 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 // Use "type: commonjs" in package.json to use CommonJS modules
 
 const express = require('express');
-
+const bodyParser = require("body-parser");
+const cors = require('cors');
 const app = express();
 
 const port = 3000;
 app.set('view engine','ejs')
 app.set('views','./views')
+
+app.use(bodyParser.json());
+/* estas linhas são para leitura de de body.json */
+app.use(express.json());
+app.use(express.urlencoded());
+/* aqui é o acesso exerno do sistema para a api */
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 require("../db/dbconnet")(app)
 require("../rotas/home")(app)
@@ -18,6 +31,7 @@ require("../rotas/getiponoticias")(app)
 require("../rotas/postnoticias")(app)
 require("../rotas/putnoticias")(app)
 require("../rotas/delnoticias")(app)
+
 
  // Define your routes
 
